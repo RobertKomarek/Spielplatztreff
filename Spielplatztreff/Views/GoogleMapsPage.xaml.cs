244e957ef895c2324a29e8e7d22ab2d8f45f6eb5
marinakomarek <robert.komarek@gmx.de>
2021-09-30 23:30:18 +0200

    Initial Commit

---
+++ Spielplatztreff/Views/GoogleMapsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Spielplatztreff.Views
+{
+    public partial class GoogleMapsPage : ContentPage
+    {
+        public GoogleMapsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
